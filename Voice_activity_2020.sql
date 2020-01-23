2ef9e6230fc254c405dfeb9549cca827d6e8634e
Reuven <Gracjan272@gmail.com>
2020-01-23 15:25:04 +0800

    Add files via upload

---
+++ Voice_activity_2020.sql
@@ -0,0 +1,60 @@
+CREATE TABLE VoiceActivity
+(
+pozycja varchar(2),
+nazwa varchar(24),
+minuty int(8)
+wynik int(8)
+);
+
+
+UPDATE VoiceActivity
+(
+SET wynik = minuty * times(int) 
+);
+
+---------------------------------------------------------
+DATE (06.01) times 1.3
+
+
+1		"Torf#6708"				2809m = 3651
+2		"Keascam#3105"			2046m = 2659
+3		"Robcio 26#9739"		2041m = 2653
+4		"bulin6#9182"			1712m = 2225
+5		"Cansisti#0447"			1682m = 2186
+6		"Reuven#2885"			1431m = 1860
+7		"Risanu#5156"			1093m = 1420
+8		"CodziennieInny#7938"	1076m = 1399
+9		"MystaLova#9881"		584m = 759
+10		"Jan#8716"				463m = 602
+
+---------------------------------------------------------
+DATE (13.01) times 1.3
+
+1       "Keascam#3105"		    2972m = 3863
+2       "bulin6#9182"    		2120m = 2756
+3       "Reuven#2885"    		1608m = 2090
+4       "MystaLova#9881"    	1509m = 1961
+5       "Robcio 26#9739"    	1418m = 1701
+6       "Cansisti#0447"    		658m = 789
+7       "CodziennieInny#7938"   532m = 638
+8       "Risanu#5156"    		504m = 655
+9       "Jan#8716"    			440m = 572
+10      "Torf#6708"    			336m = 403
+
+---------------------------------------------------------
+DATE (21.01) times 1.3
+
+1		"Keascam#3105"			2206m = 2867
+2		"bulin6#9182"			2082m = 2706
+3		"Reuven#2885"			788m = 1024
+4		"DevotedSinner#3382"	650m = 845
+5		"Robcio 26#9739"		627m = 815
+6		"Torf#6708"				597m = 776
+7		"MystaLova#9881"		542m = 704
+8		"wicio54#2985"			358m = 465
+9		"Risanu#5156"			332m = 431
+10      "CodziennieInny#7938"   260m = 338
+
+---------------------------------------------------------
+DATE (XX.XX) times 1.5
+
