5261a6365c1269fc13f69afbf3c8236aea167bdb
Reuven <Gracjan272@gmail.com>
2020-01-23 15:56:11 +0800

    Create Chat_activity_2020.sql

---
+++ Chat_activity_2020.sql
@@ -0,0 +1,140 @@
+CREATE TABLE ChatActivity
+(
+pozycja varchar(4),
+nazwa varchar(24),
+wynik int(8)
+);
+#All data provided before VC point distribution
+---------------------------------------------------------
+DATE 06.01
+
+[1]     > #Yukaire
+            Total Score: 15893     
+[2]     > #Jan
+            Total Score: 15817     
+[3]     > #Reuven
+            Total Score: 12484     
+[4]     > #HypheN
+            Total Score: 6369      
+[5]     > #Demokop
+            Total Score: 5343      
+[6]     > #Squish
+            Total Score: 4360      
+[7]     > #CodziennieInny
+            Total Score: 2477      
+[8]     > #Cansisti
+            Total Score: 2472      
+[9]     > #MiraiYukki
+            Total Score: 2287      
+[10]    > #DevotedSinner
+            Total Score: 1562 
+[11]    > #🌙 sad moon girl 🌙
+            Total Score: 1492      
+[12]    > #mermaid chan
+            Total Score: 1395      
+[13]    > #Robcio 26
+            Total Score: 1207      
+[14]    > #Torf
+            Total Score: 932       
+[15]    > #Pierozek
+            Total Score: 837       
+[16]    > #Keascam
+            Total Score: 662       
+[17]    > #Starfruit🌟
+            Total Score: 450       
+[18]    > #Ryükayaya
+            Total Score: 427       
+[19]    > #bulin6
+            Total Score: 412       
+[20]    > #ying
+            Total Score: 320       
+
+---------------------------------------------------------
+DATE 13.01
+
+[1]     > #Yukaire
+            Total Score: 25574     
+[2]     > #Jan
+            Total Score: 24780     
+[3]     > #Reuven
+            Total Score: 23044     
+[4]     > #Demokop
+            Total Score: 13523     
+[5]     > #HypheN
+            Total Score: 10072     
+[6]     > #MiraiYukki
+            Total Score: 7339      
+[7]     > #Cansisti
+            Total Score: 6194      
+[8]     > #Squish
+            Total Score: 6152      
+[9]     > #CodziennieInny
+            Total Score: 6123      
+[10]    > #bulin6
+            Total Score: 5266      
+[11]    > #Robcio 26
+            Total Score: 5031      
+[12]    > #Torf
+            Total Score: 4583      
+[13]    > #Keascam
+            Total Score: 4268      
+[14]    > #mermaid chan
+            Total Score: 3894      
+[15]    > #DevotedSinner
+            Total Score: 3628      
+[16]    > #Risanu
+            Total Score: 1561      
+[17]    > #🌙 sad moon girl 🌙
+            Total Score: 1492      
+[18]    > #Pierozek
+            Total Score: 1007      
+[19]    > #MystaLova
+            Total Score: 868       
+[20]    > #ying
+            Total Score: 605       
+
+---------------------------------------------------------
+DATE 21.01
+
+[1]     > #Reuven
+            Total Score: 33337     
+[2]     > #Yukaire
+            Total Score: 33100     
+[3]     > #Jan
+            Total Score: 29834     
+[4]     > #Demokop
+            Total Score: 19436     
+[5]     > #HypheN
+            Total Score: 12004     
+[6]     > #Squish
+            Total Score: 9946      
+[7]     > #MiraiYukki
+            Total Score: 9921      
+[8]     > #bulin6
+            Total Score: 9543      
+[9]     > #mermaid chan
+            Total Score: 9463      
+[10]    > #Keascam
+            Total Score: 8933  
+[11]    > #CodziennieInny
+            Total Score: 8568      
+[12]    > #Cansisti
+            Total Score: 7857      
+[13]    > #DevotedSinner
+            Total Score: 7777      
+[14]    > #Robcio 26
+            Total Score: 7545      
+[15]    > #Torf
+            Total Score: 5284      
+[16]    > #MystaLova
+            Total Score: 2829      
+[17]    > #Risanu
+            Total Score: 2216      
+[18]    > #🌙 sad moon girl 🌙
+            Total Score: 1492      
+[19]    > #caw.rw
+            Total Score: 1381      
+[20]    > #Pierozek
+            Total Score: 1190      
+
+
